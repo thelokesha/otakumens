9f07dba5e30af4c977eabbbc7df8ed18d969645b
LOKESH A <131357693+369loki@users.noreply.github.com>
2024-05-24 23:01:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+function sideopen(){
+    document.getElementById("showww").style.display="flex";
+}
+function sideclose(){
+    document.getElementById("showww").style.display="none";
+
+}
+function playone(){
+    document.getElementById("vidplayerone").style.display="flex";
+}
+function playtwo(){
+    document.getElementById("vidplayertwo").style.display="flex";
+}
+function playthree(){
+    document.getElementById("vidplayerthree").style.display="flex";
+}
+function playfour(){
+    document.getElementById("vidplayerfour").style.display="flex";
+}
+function playfive(){
+    document.getElementById("vidplayerfive").style.display="flex";
+}
+function playsix(){
+    document.getElementById("vidplayersix").style.display="flex";
+}
+function playseven(){
+    document.getElementById("vidplayerseven").style.display="flex";
+}
+function wnd(){
+    document.getElementById("eone").style.display="flex";
+    document.getElementById("etwo").style.display="flex";
+    document.getElementById("ethree").style.display="flex";
+    document.getElementById("efour").style.display="flex";
+    document.getElementById("efive").style.display="flex";
+    document.getElementById("esix").style.display="flex";
+    document.getElementById("eseven").style.display="flex";
+    document.getElementById("eeight").style.display="flex";
+}
+
+function eone(){
+    document.getElementById("watchone").style.display="flex";
+    document.getElementById("downloadone").style.display="flex";}
+function etwo(){
+    document.getElementById("watchtwo").style.display="flex";
+    document.getElementById("downloadtwo").style.display="flex";}
+function ethree(){
+        document.getElementById("watchthree").style.display="flex";
+        document.getElementById("downloadthree").style.display="flex";}
+function efour(){
+            document.getElementById("watchfour").style.display="flex";
+            document.getElementById("downloadfour").style.display="flex";}
+function efive(){
+                document.getElementById("watchfive").style.display="flex";
+                document.getElementById("downloadfive").style.display="flex";}
+function esix(){
+                    document.getElementById("watchsix").style.display="flex";
+                    document.getElementById("downloadsix").style.display="flex";}
+function eseven(){
+                        document.getElementById("watchseven").style.display="flex";
+                        document.getElementById("downloadseven").style.display="flex";}
+function eeight(){
+                            document.getElementById("watcheight").style.display="flex";
+                            document.getElementById("downloadeight").style.display="flex";}
+function enine(){
+                                document.getElementById("watchnine").style.display="flex";
+                                document.getElementById("downloadnine").style.display="flex";}
+function eten(){
+                                    document.getElementById("watchten").style.display="flex";
+                                    document.getElementById("downloadten").style.display="flex";}
